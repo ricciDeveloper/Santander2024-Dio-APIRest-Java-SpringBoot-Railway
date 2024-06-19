d6e1bd121fd09990cbd0e9cc29498b40e1f711b9
Joao Victor <joaovictor.ricci1999@gmail.com>
2024-06-19 15:01:25 -0300

    inicio do estudo de caso do desafio de projeto

---
+++ santanderAPIRest/src/test/java/me/dioSantander/santanderAPIRest/SantanderApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dioSantander.santanderAPIRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
